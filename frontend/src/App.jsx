2592a492f2be3202c0917d98280e587e3d3800d2
tuc <tucgiakhuong@gmail.com>
2025-04-16 21:00:25 +0700

    update header & footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayouts/MainLayouts';
 import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile.jsx';
 import './App.css'
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+        </Route>
+        <Route path="/my/profile" element={<MainLayout />}>
+          <Route index element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
