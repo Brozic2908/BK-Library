2a86edb6bc4bfe7eedb689a69a3b0db3c482b87f
MinhVuongNhat <minh.vuongnhat@hcmut.edu.vn>
2025-04-21 20:31:44 +0700

    Update MainLayouts.jsx

--- frontend/src/layouts/MainLayouts/MainLayouts.jsx
+++ frontend/src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,14 +1,17 @@
 // File: src/layouts/MainLayout.jsx
 import { Outlet } from "react-router-dom";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
       <main className="flex-grow">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
