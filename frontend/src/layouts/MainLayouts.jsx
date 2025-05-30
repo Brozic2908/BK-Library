2ecf751722bd848b90e25377c40a9a18869f6afb
Brozic2908 <166984180+Brozic2908@users.noreply.github.com>
2025-05-30 14:03:27 +0700

    complete khanh backend v1

--- frontend/src/layouts/MainLayouts.jsx
+++ frontend/src/layouts/MainLayouts.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
